471b713a9607b2559bb37e277a43f54cec707687
prebox <prebox@mail.com>
2021-12-12 14:53:59 +0100

    Use object as namespace instead of class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-import { Deep } from './deep.js'
-import { Empty } from './empty.js'
+import deepIs from './deep.js'
+import emptyIs from './empty.js'
 
-export default class How {
-  static deep = new Deep()
-  static empty = new Empty()
+/**
+ * A fast and simple helper tool.
+ * Used to call any underlying methods.
+ */
+const How = {}
 
-  /**
-   * Gets the depth of an array.
-   * 
-   * @param {Array} array The array to measure the depth of.
-   * @returns {number} A number representing the depth of the array. Returns zero if input is not of type array.
-   */
-  static deepIs(array) {
-    return this.deep.getDepth(array)
-  }
+How.deepIs = deepIs
+How.emptyIs = emptyIs
 
-  /**
-   * Gets indexes of empty values in an array. An empty value is null, undefined, an empty string or an array with a length of zero.
-   * 
-   * @param {Array} array The array to measure the emptiness of.
-   * @returns {Array} An array containing indexes of empty values.
-   */
-  static emptyIs(array) {
-    return this.empty.getIndexes(array)
-  }
-}
+Object.freeze(How)
+
+export default How
