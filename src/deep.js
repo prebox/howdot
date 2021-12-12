c9e71dfd188e6fca02018b884c8c5049644e66a8
prebox <prebox@mail.com>
2021-12-12 14:53:24 +0100

    Convert classes to functions

--- src/deep.js
+++ src/deep.js
@@ -1,17 +1,17 @@
-export class Deep {
-  /**
-   * A fast method for obtaining the depth of an array.
-   * 
-   * @param {Array} array The array to measure the depth of.
-   * @returns {number} A number representing the depth of the array.
-   */
-  getDepth(array) {
-    if (Array.isArray(array)) {
-      return 1 + array.reduce(function (max, item) {
-        return Math.max(max, item ? this.getDepth(item) : 0)
-      }.bind(this), 0)
-    } else {
-      return 0
-    }
+/**
+ * A fast method for obtaining the depth of an array.
+ * 
+ * @param {Array} array The array to measure the depth of.
+ * @returns {number} A number representing the depth of the array.
+ */
+function deepIs(array) {
+  if (Array.isArray(array)) {
+    return 1 + array.reduce(function (max, item) {
+      return Math.max(max, item ? deepIs(item) : 0)
+    }.bind(this), 0)
+  } else {
+    return 0
   }
 }
+
+export default deepIs
--- src/empty.js
+++ src/empty.js
@@ -1,25 +1,25 @@
-export class Empty {
-  /**
-   * Gets indexes of empty values in an array. An empty value is null, undefined, empty string or an array with a length of zero.
-   * 
-   * @param {Array} array The array to measure the emptiness of.
-   * @param {Array.<number>} index An array containing the actual index of the array. This is used for the recursive call and is most likely not needed on the initial call.
-   * @returns {Array} An array containing indexes of empty values.
-   */
-  getIndexes(array, index = []) {
-    index.push(0)
-		if (!array.length) return index
-    let result = []
-    while (index[index.length - 1] < array.length) {
-      let i = index[index.length - 1]
-      if (Array.isArray(array[i])) {
-        result.push.apply(result, [this.getIndexes(array[i], [...index])])
-      } else if (!array.length || [null, undefined, ''].includes(array[i])) {
-        result.push.apply(result, [[...index]])
-      }
-      index[index.length - 1]++
+/**
+ * Gets indexes of empty values in an array. An empty value is null, undefined, empty string or an array with a length of zero.
+ * 
+ * @param {Array} array The array to measure the emptiness of.
+ * @param {Array.<number>} index An array containing the actual index of the array. This is used for the recursive call and is most likely not needed on the initial call.
+ * @returns {Array} An array containing indexes of empty values.
+ */
+function emptyIs(array, index = []) {
+  index.push(0)
+  if (!array.length) return index
+  let result = []
+  while (index[index.length - 1] < array.length) {
+    let i = index[index.length - 1]
+    if (Array.isArray(array[i])) {
+      result.push.apply(result, [emptyIs(array[i], [...index])])
+    } else if (!array.length || [null, undefined, ''].includes(array[i])) {
+      result.push.apply(result, [[...index]])
     }
-    index.pop()
-    return result
+    index[index.length - 1]++
   }
+  index.pop()
+  return result
 }
+
+export default emptyIs
